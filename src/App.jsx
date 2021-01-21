590ac606082ef73104b17ac0c44d913601d83503
Luis Vazquez <luvazpa@gmail.com>
2021-01-20 22:34:03 -0600

    react hooks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import './assets/styles/App.scss';
+import Carousel from './components/Carousel';
+import CarouselItem from './components/CarouselItem';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import Search from './components/Search';
+import useInitialState from './hooks/useInitialState';
+
+const API = 'http://localhost:3000/initialState';
 
 const App = () => {
-  return <div>Hola desde App</div>;
+  const videos = useInitialState(API);
+  return (
+    <div>
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title="My list">
+          <Carousel>
+            {videos.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title="Tendencias">
+        <Carousel>
+          {videos.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Categories title="Originales de PlatziVideo">
+        <Carousel>
+          {videos.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+    </div>
+  );
 };
 
 export default App;
